aee66609ab17dc49c36bcc402ff824dcd36b1ffd
CarlosCruz8 <68169504+CarlosCruz8@users.noreply.github.com>
2020-07-11 19:26:01 -0600

    commit tarea Paises

---
+++ src/principalpaises/Canada.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principalpaises;
+
+/**
+ *
+ * @author Carlos Cruz
+ */
+public class Canada extends Pais {
+
+    @Override
+    public String getNomPais() {
+    return "Canada";
+    }
+
+    @Override
+    public String getPresidente() {
+    return "Gobernadora General Julie Payette";
+    }
+    
+}
